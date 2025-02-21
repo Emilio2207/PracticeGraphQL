dc41a2c9f3ec19e745717ed5fe763c9539a7d1ae
EmilioRamirez <emi.emilioramirez@gmail.com>
2025-02-21 16:23:24 -0300

    Query args commit

--- dist/index.js
+++ dist/index.js
@@ -1,14 +1,14 @@
-import { ApolloServer } from "@apollo/server";
-import { startStandaloneServer } from "@apollo/server/standalone";
+import { ApolloServer } from '@apollo/server';
+import { startStandaloneServer } from '@apollo/server/standalone';
 //---------------------------------------------------------------------------
 const books = [
     {
-        title: "Titulo libro 1",
-        author: "Emilio R.",
+        title: 'Titulo libro 1',
+        author: 'Emilio R.',
     },
     {
-        title: "Titulo libro 2",
-        author: "Victori V.",
+        title: 'Titulo libro 2',
+        author: 'Victori V.',
     },
 ];
 const typeDefs = `
--- index.ts
+++ index.ts
@@ -1,33 +1,38 @@
-import { ApolloServer } from "@apollo/server";
-import { Query } from "mysql2/typings/mysql/lib/protocol/sequences/Query";
-import { startStandaloneServer } from "@apollo/server/standalone";
+import { ApolloServer } from '@apollo/server';
+import { Query } from 'mysql2/typings/mysql/lib/protocol/sequences/Query';
+import { startStandaloneServer } from '@apollo/server/standalone';
 
 //---------------------------------------------------------------------------
 
 const books = [
   {
-    title: "Titulo libro 1",
-    author: "Emilio R.",
+    id: 1,
+    title: 'Titulo libro 1',
+    author: 'Emilio R.',
   },
   {
-    title: "Titulo libro 2",
-    author: "Victori V.",
+    id: 2,
+    title: 'Titulo libro 2',
+    author: 'Victori V.',
   },
 ];
 
 const typeDefs = `
     type Book {
+        id:ID
         title: String
         author: String
     }
     type Query {
         books:[Book]
+        book(id:ID!): Book
     }
 `;
 
 const resolvers = {
   Query: {
     books: () => books,
+    book: (parent, args) => books.find((book) => book.id === parseInt(args.id)),
   },
 };
 
